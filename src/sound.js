10de8b48a8eb69ca80e7c5ac14ec8c77dba384d4
Elias Lehnert <araknor99@Eliass-MacBook-Pro.local>
2018-10-20 17:55:06 +0200

    fixed merge conflict

--- src/sound.js
+++ src/sound.js
@@ -1,13 +1,19 @@
 class Sound{
-	constructor(soundFile,posX,posY){
+	constructor(soundFile,posX,posY,repeat){
 		this.mSound = soundFile;
 		this.mPanner = p5.Panner3D();
 		this.mX = posX;
 		this.mY = posY;
+		this.mRepeat = repeat
 	}
 
 	function play(){
-		if(!this.mSound.isPlaying())
-			this.mSound.play();
+		if(!mRepeat){
+			if(!this.mSound.isPlaying())
+				this.mSound.play();
+		}else{
+			if(!this.mSound.isLooping())
+				this.mSOund.loop()
+		}
 	}
 }
