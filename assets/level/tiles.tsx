2e43a3a6d518f3c82a4a3d35c54d72e893500b1a
Jaro Habiger <jarohabiger@googlemail.com>
2018-10-20 22:45:43 +0200

    add sounds + more

--- assets/level/tiles.tsx
+++ assets/level/tiles.tsx
@@ -1,4 +1,4 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.1" tiledversion="2018.09.12" name="Hölle" tilewidth="16" tileheight="16" tilecount="552" columns="23">
- <image source="dungeon_tiles (1).png" width="368" height="384"/>
+<tileset version="1.2" tiledversion="1.2.0" name="cave" tilewidth="16" tileheight="16" tilecount="552" columns="23">
+ <image source="tiles.png" width="368" height="384"/>
 </tileset>
--- src/mapview.js
+++ src/mapview.js
@@ -4,11 +4,11 @@ var x = 0, y = 0;
 var showCoords = false;
 
 function preload() {
-  tmap = loadTiledMap("map");
+  tmap = loadTiledMap('map');
 }
 
 function setup() {
-  preload()
+  preload();
   createCanvas(800, 600);
   ellipseMode(CENTER);
   s = tmap.getMapSize();
@@ -28,7 +28,7 @@ function draw() {
     }
   }
   textSize(24);
-  text("ADWS to move, Z to toogle Map Coordinates, C to change map", 10, 50);
+  text("ADWS to move, Z to toogle Map Coordinates", 10, 50);
   text("Corner: Canvas coordinates: " + x + ", " + y, 10, 100);
   text("Corner: Map coordinates: " + tmap.canvasToMap(x, y).x + ", " + tmap.canvasToMap(x, y).y, 10, 150);
   text("Map Name: " + tmap.getName(), 10, 200);
@@ -52,48 +52,6 @@ function draw() {
 
 function keyPressed(){
   if(key == 'z' || key == 'Z') showCoords = !showCoords;
-  if(key == 'c' || key == 'C') changeMap();
-}
-
-function changeMap(){
-  x = 0;
-  y = 0;
-  switch(tmap.getName()){
-    case "desert":
-      // tmap = loadTiledMap("isometric_grass_and_water", "data"); might work here
-      // but callback functions are soooo cool!
-      loadTiledMap("isometric", "data", mapLoaded);
-      fill(255);
-      break;
-    case "isometric":
-      loadTiledMap("staggered_x_even", "data", mapLoaded);
-      fill(255);
-      break;
-    case "staggered_x_even":
-      loadTiledMap("staggered_x_odd", "data", mapLoaded);
-      fill(255);
-      break;
-    case "staggered_x_odd":
-      loadTiledMap("staggered_y_even", "data", mapLoaded);
-      fill(255);
-      break;
-    case "staggered_y_even":
-      loadTiledMap("staggered_y_odd", "data", mapLoaded);
-      fill(255);
-      break;
-    case "staggered_y_odd":
-      loadTiledMap("hexagonal", "data", mapLoaded);
-      fill(0);
-      break;
-    case "hexagonal":
-      loadTiledMap("perspective_walls", "data", mapLoaded);
-      fill(0);
-      break;
-    case "perspective_walls":
-      loadTiledMap("desert", "data", mapLoaded);
-      fill(0);
-      break;
-  }
 }
 
 function mapLoaded(newMap) {
