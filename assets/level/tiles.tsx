de738432fd44112f34afa2bd1552ab38e84d8f54
Elias Lehnert <araknor99@Eliass-MacBook-Pro.local>
2018-10-20 20:42:25 +0200

    Revert "Ich habe Jago zu frieden gestellt."
    This reverts commit 3ad4991b2fcbca673ec1a37a58d4cd6f2af0aded.

--- assets/level/tiles.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.1" tiledversion="2018.09.12" name="Hölle" tilewidth="16" tileheight="16" tilecount="552" columns="23">
- <image source="dungeon_tiles (1).png" width="368" height="384"/>
-</tileset>
