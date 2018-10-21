7746591b7c9a2ee981551473687be42b6caa0d03
Luca Kucznierz <lucakucznierz@Lucas-MacBook-Pro.local>
2018-10-21 02:56:00 +0200

    audio manager working

--- src/main.js
+++ src/main.js
@@ -1,74 +1,71 @@
-class GameObject{	
-	constructor(x,y){       
-		this.mX = x;
-		this.mY = y;
-        this.mSound = sound;
-    }
+class GameObject {
+  constructor(x, y) {
+    this.mX = x;
+    this.mY = y;
+    this.mSound = sound;
+  }
 }
 
-class SoundManager{
-    constructor(){
-		this.mSounds = [];
-	}
+class SoundManager {
+  constructor() {
+    this.mSounds = [];
+  }
 
-    playSound(sound){
-        this.mSounds.push(sound);
-    }
+  playSound(sound) {
+    this.mSounds.push(sound);
+  }
 
-    manageSounds(){
-        for(i = 0; i < mSounds.length; i++){
-            if(!mSound[i].mFinished)
-                sound.play();
-            else
-                removeSound(mSound[i]);
-        }
-    }
+  manageSounds() {
+    this.mSounds.forEach(sound => {
+      if (!sound.mFinished)
+        sound.play();
+      else
+        this.removeSound(sound.special_snowflake);
+    })
+  }
 
-    cancelAllSounds(){
-        for(i = 0; i < mSounds.length(); i++){
-            mSounds[i].stop();
-        }
-    }   
-  
-    removeSound(sound){
-        newArr = [];
-        for(i = 0; i < mSounds.length();i++){
-            if(mSounds[i] != sound)
-                newArr.push(mSounds)
-			else
-				mSounds[i].stop()
-        }
-        mSounds = newArr;
-    }
+  cancelAllSounds() {
+    this.mSounds.forEach(x => this.removeSound(x.special_snowflake))
+  }
+
+  removeSound(special_snowflake) {
+    this.mSounds.filter(x => x.special_snowflake == special_snowflake)[0].mSound.pause();
+    this.mSounds = this.mSounds.filter(x => x.special_snowflake != special_snowflake);
+  }
 }
 
-class Sound{
-    constructor(soundFile,posX,posY,panner,repeat){
-        this.mSound = soundFile;
-        this.mPanner = panner;
-        this.x = posX;
-		this.y = posY;
-        this.mRepeat = repeat;
-        this.mFinished = false;
-		this.mSound.disconnect();
-		this.mSound.connnect(mPanner);
+class Sound {
+  constructor(soundFile, posX, posY, panner, repeat) {
+    this.special_snowflake = Math.random().toString(36);
+    this.mSound = soundFile;
+    this.mPanner = panner;
+    this.x = posX;
+    this.y = posY;
+    this.mRepeat = repeat;
+    this.mFinished = false;
+    this.mSound.disconnect();
+    this.mSound.connect(this.mPanner);
+
+
+    this.removeWrapper = function() {
+      var e = function(sound, special_snowflake) {
+        sound.mFinished = true;
+        soundManager.removeSound(sound.special_snowflake);
+      }
+      e(this, this.special_snowflake);
     }
 
-    play(){
-        if(!mRepeat){
-            if(!this.mSound.isPlaying()){
-                this.mSound.play();
-                setTimeout(finish,this.mSound.duration())
-            }
-        }else{
-            if(!this.mSound.isLooping())
-                this.mSound.loop();
-        }
-    }
+  }
 
-    finish(){
-        this.mFinished = true
+
+
+  play() {
+    if (!this.mSound.isPlaying()) {
+      this.mSound.play();
+      if (!this.mRepeat)
+        setTimeout(() => this.removeWrapper(), this.mSound.buffer.duration * 1000);
     }
+  }
 }
 
 var v_pos = new p5.Vector(0, 0);
@@ -79,21 +76,32 @@ var y = -200;
 var angle = 90;
 var soundManager = new SoundManager();
 
+function setup() {
+
+
+  panner1 = new p5.Panner3D();
+  angleMode(DEGREES);
+
+  createCanvas(windowWidth, windowHeight);
+
+}
+
 function preload() {
 
   sound = loadSound('/assets/sound/steps-gravel.mp3');
   panner1 = new p5.Panner3D();
+  soundObject = new Sound(sound, 0, 0, panner1, false);
+  soundManager.playSound(soundObject);
   angleMode(DEGREES);
   sound.disconnect()
   sound.connect(panner1)
-
 }
 
 function draw() {
 
 
   soundManager.manageSounds();
-  translate(windowWidth/2, windowHeight/2);
+  translate(windowWidth / 2, windowHeight / 2);
 
   rectMode(CENTER);
 
@@ -101,46 +109,47 @@ function draw() {
 
   push()
   rotate(-angle + 90);
-  translate(-v_pos.x,-v_pos.y);
+  translate(-v_pos.x, -v_pos.y);
 
   v_dir = createVector(cos(angle), sin(angle));
 
-  player = rect(v_pos.x,v_pos.y, 10, 10);
+  player = rect(v_pos.x, v_pos.y, 10, 10);
   obj1 = ellipse(50, 0, 10, 10);
 
   if (keyIsDown(LEFT_ARROW)) {
     angle -= 1
-    sound.disconnect()
-    sound.play()
-    sound.connect(panner1)
+    // sound.disconnect()
+    // sound.play()
+    // sound.connect(panner1)
   }
   if (keyIsDown(RIGHT_ARROW)) {
     angle += 1
-    sound.disconnect()
-    sound.play()
-    sound.connect(panner1)
+    // sound.disconnect()
+    // sound.play()
+    // sound.connect(panner1)
   }
   if (keyIsDown(DOWN_ARROW)) {
     v_pos.add(v_dir.mult(2))
-    sound.disconnect()
-    sound.play()
-    sound.connect(panner1)
+    // sound.disconnect()
+    // sound.play()
+    // sound.connect(panner1)
   }
   if (keyIsDown(UP_ARROW)) {
     v_pos.sub(v_dir.mult(2))
-    sound.disconnect()
-    sound.play()
-    sound.connect(panner1)
+    // sound.disconnect()
+    // sound.play()
+    // sound.connect(panner1)
   }
 
   v_panner = new p5.Vector(50, 0);
 
   v_panner.sub(v_pos);
-  v_panner.rotate((-angle + 90)/180*PI);
+  v_panner.rotate((-angle + 90) / 180 * PI);
 
-  panner1.positionX(v_panner.x*30)
-  panner1.positionY(v_panner.y*30)
+  panner1.positionX(v_panner.x * 20)
+  panner1.positionY(v_panner.y * 20)
 
-  console.log(v_panner.x, v_panner.y)
+  //  console.log(v_panner.x, v_panner.y)
+
 
 }
